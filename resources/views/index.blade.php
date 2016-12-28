7c71b70ab966859b2004d78a8e8eb4f10e849c9a
Lean Sibal <leansibal@gmail.com>
2016-12-29 03:11:16 +0800

    Setting of client current location, and default location to Manila

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,9 +24,15 @@
       var map;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
+          center: {lat: 14.6091, lng: 121.0223},
+          zoom: 13
         });
+		if (typeof navigator !== 'undefined' && typeof navigator.geolocation !== 'undefined' ) {
+			 navigator.geolocation.getCurrentPosition(function (position) {
+				 initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+				 map.setCenter(initialLocation);
+			 });
+		 }
       }
     </script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAxkBB9mkoz-AuZJ2yhN17R00DHpMQYkog&callback=initMap"
